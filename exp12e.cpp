620aa7df4e9ba48cdcba6e9076c4dc9fe7251b4f
vanshika-singh14 <vanshikajksingh1405@gmail.com>
2024-10-06 17:42:47 +0530

    Create exp12e.cpp

---
+++ exp12e.cpp
@@ -0,0 +1,51 @@
+//Vanshika Singh
+//23070123148
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+//types of constructors - copy constructor
+class student
+{
+    int rn;
+    char n[50];
+    float avg = 0;
+    public:
+    student(int,char[],float);
+
+    student(student &t)
+    {
+        rn=t.rn;
+        strcpy(n,t.n);
+        avg=t.avg;
+    }
+    void display();
+
+};
+
+ student::student(int rno,char na[],float av)
+ {
+    rn=rno;
+    strcpy(n,na);
+    avg=av;
+ }
+
+void student::display()
+ {
+    cout<<endl;
+    cout<<"Name: "<<n<<endl;
+    cout<<"Roll No: "<<rn<<endl;
+    cout<<"Average: "<<avg<<" %"<<endl;
+ }
+
+int main()
+{
+    student s1(45,"Trijal",93.7);
+    s1.display();
+
+    student trijal(s1);
+    trijal.display();
+
+    return 0;
+}
